f5de60f67a96d6e5cc047ed438230aaeabd20b99
tuananht21 <tuananhcepdn@gmail.com>
2026-09-14 10:40:12 +0700

    feat: thêm validate cho danh mục sản phẩm

---
+++ backend/app/Http/Requests/category/categoryValidation.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\category;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class categoryValidation extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'name' => 'required|string|max:255',
+            'slug' => 'required|string|max:255|unique:categories,slug',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Name is required.',
+            'name.max' => 'Name must not exceed 255 characters.',
+            'slug.required' => 'Slug is required.',
+            'slug.max' => 'Slug must not exceed 255 characters.',
+            'slug.unique' => 'Slug already exists.',
+        ];
+    }
+}
