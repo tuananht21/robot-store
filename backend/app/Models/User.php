d2d6f614980262cecce24c45d6567c57926061b5
tuananht21 <tuananhcepdn@gmail.com>
2026-09-15 13:39:44 +0700

    fix: sử dụng hash phía controller

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -56,7 +56,6 @@ class User extends Authenticatable implements JWTSubject
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
         ];
     }
 
